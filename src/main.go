269ee302e2d86fa3199ac54aec4a3eb95b185f94
patrickap <patrick.schlageter@web.de>
2023-01-28 23:03:32 +0100

    refactor: improved script and names

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/rwcarlsen/goexif/exif"
@@ -22,25 +23,30 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Recursively read source directory
 	err := filepath.Walk(*source, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
+		// Skip directories, files only
 		if info.IsDir() {
 			return nil
 		}
 
-		if !isValidExtension(path) {
+		// Allow valid file extensions only
+		if !isValidExt(path) {
 			return nil
 		}
 
-		fileTime, err := extractExifDateTime(path)
+		fileTime, err := decodeExifTime(path)
+		// If no exif data is available move file to 'unknown' directory
 		if err != nil {
 			newPath := filepath.Join(*target, "unknown", filepath.Base(path))
 			return moveFile(path, newPath)
 		}
 
+		// Rename file and move it to target directory
 		yearDir := fmt.Sprintf("%d", fileTime.Year())
 		monthDir := fmt.Sprintf("%d-%02d", fileTime.Year(), fileTime.Month())
 		fileName := fmt.Sprintf("%d-%02d-%02d_%02d.%02d.%02d%s", fileTime.Year(), fileTime.Month(), fileTime.Day(), fileTime.Hour(), fileTime.Minute(), fileTime.Second(), filepath.Ext(path))
@@ -56,11 +62,11 @@ func main() {
 	}
 }
 
-func isValidExtension(path string) bool {
-	enxtensions := []string{".tiff", ".tif", ".gif", ".jpeg", ".jpg", ".png", ".img", ".bmp", ".raw", ".heif", ".heic", ".mkv", ".avi", ".mov", ".wmv", ".mp4", ".m4v", ".mpg", ".mpeg", ".hevc"}
+func isValidExt(path string) bool {
+	validExts := []string{".tiff", ".tif", ".gif", ".jpeg", ".jpg", ".png", ".img", ".bmp", ".raw", ".heif", ".heic", ".mkv", ".avi", ".mov", ".wmv", ".mp4", ".m4v", ".mpg", ".mpeg", ".hevc"}
 
-	for _, e := range enxtensions {
-		if filepath.Ext(path) == e {
+	for _, ext := range validExts {
+		if filepath.Ext(path) == ext {
 			return true
 		}
 	}
@@ -68,24 +74,28 @@ func isValidExtension(path string) bool {
 	return false
 }
 
-func extractExifDateTime(path string) (time.Time, error) {
+func decodeExif(path string) (*exif.Exif, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return time.Time{}, err
+		return nil, err
 	}
 	defer file.Close()
 
-	x, err := exif.Decode(file)
+	return exif.Decode(file)
+}
+
+func decodeExifTime(path string) (time.Time, error) {
+	exifData, err := decodeExif(path)
 	if err != nil {
 		return time.Time{}, err
 	}
 
-	tm, err := x.DateTime()
+	exifTime, err := exifData.DateTime()
 	if err != nil {
 		return time.Time{}, err
 	}
 
-	return tm, nil
+	return exifTime, nil
 }
 
 func moveFile(sourceFile, targetFile string) error {
@@ -94,13 +104,14 @@ func moveFile(sourceFile, targetFile string) error {
 		return err
 	}
 
-	// Check if target file already exists
+	// If target file already exists, append a postfix
 	if _, err := os.Stat(targetFile); err == nil {
-		// Target file already exists, append a postfix
-		ext := filepath.Ext(targetFile)
-		base := targetFile[:len(targetFile)-len(ext)]
+		fileExt := filepath.Ext(targetFile)
+		fileBase := filepath.Base(targetFile)
+		fileName := strings.TrimSuffix(fileBase, fileExt)
+
 		for i := 1; ; i++ {
-			newTargetFile := fmt.Sprintf("%s-%d%s", base, i, ext)
+			newTargetFile := fmt.Sprintf("%s-%d%s", fileName, i, fileExt)
 			if _, err := os.Stat(newTargetFile); os.IsNotExist(err) {
 				targetFile = newTargetFile
 				break
